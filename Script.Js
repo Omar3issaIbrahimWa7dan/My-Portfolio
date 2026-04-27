70d97cd288522f0cd56ffc088a090f2870a69b98
Omar Eissa ibrahim Wahdan <wahdanomareissa@gmail.com>
2026-04-28 01:31:36 +0300

    Delete Script.Js

--- Script.Js
+++
@@ -1,7 +0,0 @@
-let manuicon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
-
-manuicon.onclick = () => {
-    manuicon.classList.toggle('bx-x')
-    navbar.classList.toggle('active');
-}
